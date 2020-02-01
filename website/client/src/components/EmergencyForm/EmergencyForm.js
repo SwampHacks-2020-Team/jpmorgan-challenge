94d86b35646d21bb44b84b1e10799fa5e5693307
Carlos Carrasquillo <c.carrasquillo@ufl.edu>
2020-02-01 13:53:45 -0500

    finished home page form and implemented regex expressions for emergency form

--- website/client/src/components/EmergencyForm/EmergencyForm.js
+++ website/client/src/components/EmergencyForm/EmergencyForm.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import "./EmergencyForm.css";
 const GetHelp = require('./GetHelp.js');
 
-/* Using Tutorial: https://www.youtube.com/watch?v=4CeTFW4agRw by Brice Ayres*/
-
 const phoneRegex = RegExp(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/);
 const partySizeRegex = RegExp(/^[1-9][0-9]?$/);
+const longitudeRegex = RegExp(/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$/);
+const latitudeRegex = longitudeRegex;
 
 const formValid = ({ formErrors, ...rest }) => {
     let valid = true;
@@ -26,8 +26,10 @@ class App extends Component {
         this.state = {  name:       null,
                         phone:      null,
                         partySize:  null,
+                        longitude:  null,
+                        latitude:   null,
                         message:    null,
-                        formErrors: { name: "", partySize: "", phone: "", message: "" },
+                        formErrors: { name: "", partySize: "", phone: "", longitude: "", latitude: "", message: "" },
                         success:    false
                      };
     }
@@ -40,6 +42,8 @@ class App extends Component {
             GetHelp.getHelp(this.state.name,
                             this.state.partySize,
                             this.state.phone, 
+                            this.longitude,
+                            this.latitude,
                             this.state.message)
             this.setState({success: true});
         }
@@ -58,13 +62,16 @@ class App extends Component {
                 formErrors.name = value.length < 3 ? "minimum 3 characaters required" : "";
             break;
             case "partySize":
-                formErrors.partySize = partySizeRegex.test(value) ? "not a valid number of people" : "";
+                formErrors.partySize = partySizeRegex.test(value) ? "" : "not a valid number of people";
             break;
             case "phone":
                 formErrors.phone = phoneRegex.test(value) ? "" : "invalid phone number";
             break;
-            case "message":
-                formErrors.message = value.length < 10 ? "minimum 10 characaters required" : "";
+            case "longitude":
+                formErrors.longitude = longitudeRegex.test(value) ? "" : "invalid longitude";
+            break;
+            case "latitude":
+                formErrors.latitude = latitudeRegex.test(value) ? "" : "invalid latitude";
             break;
             default:
             break;
@@ -93,9 +100,9 @@ class App extends Component {
                     </div>
 
                     <div className="party-size">
-                        <label htmlFor="partySize">Party Size</label>
+                        <label htmlFor="partySize">Number Stranded</label>
                         <input className={formErrors.partySize.length > 0 ? "error" : null}
-                               placeholder="Last Name"
+                               placeholder="0-99"
                                type="text"
                                name="partySize"
                                noValidate
@@ -118,10 +125,36 @@ class App extends Component {
                         )}
                     </div>
 
+                    <div className="longitude">
+                        <label htmlFor="longitude">Longitude</label>
+                        <input className={formErrors.longitude.length > 0 ? "error" : null}
+                               placeholder="0.000000"
+                               type="longitude"
+                               name="longitude"
+                               noValidate
+                               onChange={this.handleChange}/>
+                        {formErrors.longitude.length > 0 && (
+                            <span className="errorMessage">{formErrors.longitude}</span>
+                        )}
+                    </div>
+
+                    <div className="latitude">
+                        <label htmlFor="latitude">Latitude</label>
+                        <input className={formErrors.latitude.length > 0 ? "error" : null}
+                               placeholder="0.000000"
+                               type="latitude"
+                               name="latitude"
+                               noValidate
+                               onChange={this.handleChange}/>
+                        {formErrors.phone.length > 0 && (
+                            <span className="errorMessage">{formErrors.latitude}</span>
+                        )}
+                    </div>
+
                     <div className="message">
-                        <label htmlFor="message">Message</label>
+                        <label htmlFor="message">Tell us how we can help.</label>
                         <textarea className={formErrors.message.length > 0 ? "error" : null}
-                               placeholder="Message"
+                               placeholder="Inform us of any outstanding conditions."
                                type="message"
                                name="message"
                                noValidate
