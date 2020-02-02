fc813adf4b3fffd04b161108946201f0a8ed6d33
Jonathan Lau <jonathanlau1@hotmail.com>
2020-02-01 23:20:30 -0500

    updated FAQ

--- website/client/src/components/FAQ/FAQ.js
+++ website/client/src/components/FAQ/FAQ.js
@@ -17,37 +17,48 @@ class FAQ extends React.Component {
                 <div className="accordion">
                     <div className="accordion-item" id="question1">
                         <a className="accordion-link" href="#question1">
-                            <span>Question 1?</span>
+                            <span>How can I request help?</span>
                             <i className="icon ion-md-add"/>
                             <i className="icon ion-md-remove"/>
                         </a>
                         <div className="answer">
                             <p>
-                                Answer 1.
+                                Navigate to the "Get Help" page and enter your contact
+                                information and coordinates (or use our built-in feature
+                                to get your coordinates). Be sure to inform us of any
+                                obstacles near your location that may prevent us from
+                                reaching you so that our system can plan accordingly.
                             </p>
                         </div>
                     </div>
                     <div className="accordion-item" id="question2">
                         <a className="accordion-link" href="#question2">
-                            <span>Question 2?</span>
+                            <span>How does RTRescue work?</span>
                             <i className="icon ion-md-add"/>
                             <i className="icon ion-md-remove"/>
                         </a>
                         <div className="answer">
                             <p>
-                                Answer 2.
+                                Once your location is added to the queue, our system
+                                uses a genetic algorithm to determine a near-optimal route
+                                for ordering the different locations in the queue to
+                                minimize total travel distance. Topographical data is analyzed in
+                                conjuction with flood water depths to determine the exact path
+                                of the boat to various ordered waypoints.
+
                             </p>
                         </div>
                     </div>
                     <div className="accordion-item" id="question3">
                         <a className="accordion-link" href="#question3">
-                            <span>Question 3?</span>
+                            <span>I just submitted my location. What happens now?</span>
                             <i className="icon ion-md-add"/>
                             <i className="icon ion-md-remove"/>
                         </a>
                         <div className="answer">
                             <p>
-                                Answer 3.
+                            Your contact information and location have been added into a queue
+                            and a volunteer will be arriving soon to assist you.
                             </p>
                         </div>
                     </div>
@@ -59,7 +70,7 @@ class FAQ extends React.Component {
                         </a>
                         <div className="answer">
                             <p>
-                                Answer 4.
+                            Answer 4.
                             </p>
                         </div>
                     </div>
